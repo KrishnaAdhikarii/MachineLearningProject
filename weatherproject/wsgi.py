d7ddc583283505eec2741852f1d16f3107b51a43
321506402027 <321506402027@andhrauniversity.edu.in>
2025-04-10 13:21:52 +0530

    Update wsgi.py

--- weatherproject/wsgi.py
+++ weatherproject/wsgi.py
@@ -8,11 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'django' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherproject.settings')
 
+# Expose the WSGI application to be used by the WSGI server
 application = get_wsgi_application()
-
-app = application
